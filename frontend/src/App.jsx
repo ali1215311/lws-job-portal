c6e020506530965db972fc0ced8e38a8af106052
Mohammad Ali <mohammad.ali1215311@gmail.com>
2026-03-02 16:39:53 +0600

    Fix server JWT token issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,28 @@
+import axios from "axios";
 const App = () => {
+  const handleClick = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:9000/api/auth/login",
+        {
+          email: "john.carter@example.com",
+          password: "SecurePass123!",
+          role: "USER",
+        },
+      );
+      if (response.status === 200) {
+        console.log(response.data);
+      }
+    } catch (error) {
+      console.log(error.response?.data);
+    }
+  };
+
   return (
     <>
-      <div className="bg-red-200">App</div>
+      <div className="bg-red-200">
+        <button onClick={handleClick}>Click</button>
+      </div>
     </>
   );
 };
